f056883ab2ab9ca02329016763265d5c6aa88ba5
nkhunters <nkhunters07@gmail.com>
2023-11-21 21:54:37 +0400

    initial commit

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,11 @@
+import { Input } from "@/components/ui/input";
+
+const Login = () => {
+  return (
+    <div>
+      Login Page
+      <Input type="email" placeholder="Email" />
+    </div>
+  );
+};
+export default Login;
